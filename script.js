3a03a69afb1a1b81d31b62a0e3b7f3e60174cf2f
parora54 <pranava880@gmail.com>
2024-07-03 16:45:24 -0400

    Fixed all code

--- script.js
+++ script.js
@@ -15,5 +15,60 @@ function getComputerChoice() {
     return choice;
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    let user = String(prompt("Type 'rock', 'paper', or 'scissors'."));
+    user = user.toLowerCase();
 
+    switch (user) {
+        case "rock":
+            return "rock"
+        case "scissors":
+            return "scissors"
+        case "paper":
+            return "paper"
+        default:
+            getHumanChoice();
+    }
+}
+
+function playGame() {
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        console.log(`You chose: ${humanChoice}`);
+        console.log(`Computer chose: ${computerChoice}`);
+    
+        if (
+            (humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper")
+        ) {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+        } else if (humanChoice === computerChoice) {
+            console.log("It's a tie!");
+        } else {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore++;
+        }
+    
+        console.log(`Score - You: ${humanScore}, Computer: ${computerScore}`);
+    }
+
+    while (humanScore < 3 && computerScore < 3) {
+        const cpu = getComputerChoice()
+        const human = getHumanChoice()
+        playRound(human, cpu)
+    }
+
+    if (humanScore == 3) {
+        console.log("GAME OVER, PLAYER WINS!!!")
+    }
+    else {
+        console.log("GAME OVER, CPU WINS!!!")
+    }
+}
+
+playGame();
